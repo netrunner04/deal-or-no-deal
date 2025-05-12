584f0beff841a5044509ee93eb7014de0587eb37
Florian Jahnke <103238566+netrunner04@users.noreply.github.com>
2025-05-12 10:05:36 +0200

    Update deal_or_no_deal.java

--- deal_or_no_deal.java
+++ deal_or_no_deal.java
@@ -79,7 +79,6 @@ public class deal_or_no_deal
     catch(FileNotFoundException e2)
     {
       System.out.println("An error occurred. File not found.");
-      //System.out.println("Willkommen zum Spiel Deal or No Deal:\n\nDie Spielbeschreibung:\nBei Deal or No Deal spielt ein Kandidat gegen eine Bank.\nDie Bank hat Geldkoffer, deren Beträge bekannt sind und auf einer Tafel angezeigt werden.\nDer Kandidat wählt einen Koffer und einige weitere Koffer zum Öffnen. Die Beträge in den geöffneten Koffern werden auf der Tafel gestrichen.\nDie Bank macht ein Angebot für den Koffer und wenn der Kandidat das Angebot annimmt ist das Spiel zu Ende.\nDanach geht es in Runden von Kofferöffnungen und Bankangebot weiter, bis der Kandidat das Angebot annimmt oder bis er den Inhalt des letzten Koffers erhält.\n");
       e2.printStackTrace();
     }
 
