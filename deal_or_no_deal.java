2e3b5d049092189c0074c07df417a1ef81e8ce89
Florian Jahnke <103238566+netrunner04@users.noreply.github.com>
2025-05-12 09:06:54 +0200

    Update deal_or_no_deal.java
    added table method

--- deal_or_no_deal.java
+++ deal_or_no_deal.java
@@ -42,6 +42,7 @@ public class deal_or_no_deal
       {
           System.out.println("Spiel startet:");
           //call methods for the game content
+          table();
           break;
       }
 
@@ -56,6 +57,41 @@ public class deal_or_no_deal
         System.out.println("wrong input\n");
       }
     }
+  }
+
+  public static void table()
+  {
+    System.out.println("Please choose a value:");
+
+    try
+      {
+        File valueInstr = new File("values_instruction.txt");
+        Scanner valueinstrReader = new Scanner(valueInstr);
+
+        while(valueinstrReader.hasNextLine())
+          {
+            String valueinstrContent = valueinstrReader.nextLine();
+            System.out.println(valueinstrContent);
+          }
+        valueinstrReader.close();
+      }
+
+    catch(FileNotFoundException e2)
+    {
+      System.out.println("An error occurred. File not found.");
+      //System.out.println("Willkommen zum Spiel Deal or No Deal:\n\nDie Spielbeschreibung:\nBei Deal or No Deal spielt ein Kandidat gegen eine Bank.\nDie Bank hat Geldkoffer, deren Beträge bekannt sind und auf einer Tafel angezeigt werden.\nDer Kandidat wählt einen Koffer und einige weitere Koffer zum Öffnen. Die Beträge in den geöffneten Koffern werden auf der Tafel gestrichen.\nDie Bank macht ein Angebot für den Koffer und wenn der Kandidat das Angebot annimmt ist das Spiel zu Ende.\nDanach geht es in Runden von Kofferöffnungen und Bankangebot weiter, bis der Kandidat das Angebot annimmt oder bis er den Inhalt des letzten Koffers erhält.\n");
+      e2.printStackTrace();
+    }
+
+    Scanner readChoice = new Scanner(System.in);
+    int valueChoice = readChoice.nextInt();
+    readChoice.close();
+    System.int.println(valueChoice);
+  }
+  
+  public static void offer_calc()
+  {
+    System.out.println("offer calculation:");
   }
 
   public static void main(String[] args)
