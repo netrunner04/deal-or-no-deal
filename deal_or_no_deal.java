936cdd431302bc1a14de04543f116042d1cb8927
Florian Jahnke <103238566+netrunner04@users.noreply.github.com>
2025-05-12 21:21:07 +0200

    fixed bug in deal_or_no_deal.java
    - Error with System.in fixed. The scanner was closed too early.

--- deal_or_no_deal.java
+++ deal_or_no_deal.java
@@ -23,26 +23,21 @@ public class deal_or_no_deal
     catch(FileNotFoundException e)
     {
       System.out.println("An error occurred. File not found.");
-      //System.out.println("Willkommen zum Spiel Deal or No Deal:\n\nDie Spielbeschreibung:\nBei Deal or No Deal spielt ein Kandidat gegen eine Bank.\nDie Bank hat Geldkoffer, deren Beträge bekannt sind und auf einer Tafel angezeigt werden.\nDer Kandidat wählt einen Koffer und einige weitere Koffer zum Öffnen. Die Beträge in den geöffneten Koffern werden auf der Tafel gestrichen.\nDie Bank macht ein Angebot für den Koffer und wenn der Kandidat das Angebot annimmt ist das Spiel zu Ende.\nDanach geht es in Runden von Kofferöffnungen und Bankangebot weiter, bis der Kandidat das Angebot annimmt oder bis er den Inhalt des letzten Koffers erhält.\n");
       e.printStackTrace();
     }
   }
 
-  public static void user_choice()
+  public static void user_choice(Scanner user_input)
   {
     System.out.println("\nWas moechten Sie tun:\n\nSpiel starten = 1\nAnwendung beenden = 2\nBitte geben Sie ihre Auswahl ein: ");
-      
-    Scanner choice = new Scanner(System.in);
-    int user_choice = choice.nextInt();
-    choice.close();
+    int user_choice = user_input.nextInt();
 
     switch(user_choice)
     {
       case 1:
       {
           System.out.println("Spiel startet:");
-          //call methods for the game content
-          table();
+          table(user_input);
           break;
       }
 
@@ -59,9 +54,9 @@ public class deal_or_no_deal
     }
   }
 
-  public static void table()
+  public static void table(Scanner user_input)
   {
-    System.out.println("Please choose a value:");
+    System.out.println("\nSuitcases:");
 
     try
       {
@@ -81,11 +76,9 @@ public class deal_or_no_deal
       System.out.println("An error occurred. File not found.");
       e2.printStackTrace();
     }
-
-    Scanner readChoice = new Scanner(System.in);
-    int valueChoice = readChoice.nextInt();
-    readChoice.close();
-    System.out.println(valueChoice);
+  
+    System.out.println("\nPlease choose a Suitcase:");
+    int valueChoice = user_input.nextInt();
   }
   
   public static void offer_calc()
@@ -94,8 +87,12 @@ public class deal_or_no_deal
   }
 
   public static void main(String[] args)
-  {      
+  {   
+    Scanner user_input = new Scanner(System.in);
+
     instruction_file_read();
-    user_choice();
+    user_choice(user_input);
+
+    user_input.close();
   }
 }
