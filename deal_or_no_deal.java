358c2fa23955d8f24175ac1c0e0ae2e7584c0cde
Florian Jahnke <103238566+netrunner04@users.noreply.github.com>
2025-05-13 16:14:09 +0200

    added stuff in deal_or_no_deal.java
    - added an array for the values in the cases
    - added the generation of the output that creates the table with the 
      values

--- deal_or_no_deal.java
+++ deal_or_no_deal.java
@@ -1,5 +1,6 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.Random;
 import java.util.Scanner;
 
 public class deal_or_no_deal
@@ -27,6 +28,57 @@ public class deal_or_no_deal
     }
   }
 
+  public static void suitcaseValues(int[] valuesSuitcases)
+  {
+    int index = 0,arraylength = valuesSuitcases.length;
+    while (index < arraylength) 
+    {
+      int rNumber = 0;
+
+      Random r_generator = new Random();
+      rNumber = r_generator.nextInt(899999) + 100000;
+
+      valuesSuitcases[index] = rNumber;
+      index++;
+    }
+  }
+
+  public static void table(Scanner user_input)
+  {
+    int[] valuesSuitcases = new int[30];
+    int i,suitcase_number = 1;
+    suitcaseValues(valuesSuitcases);
+
+    System.out.println("\nSuitcases:");
+    System.out.println("------------------------------------------------------------------------------------------------");
+    System.out.print("| ");
+    for(i=0;i < valuesSuitcases.length;i++)
+    {
+      if(suitcase_number < 10)
+      {
+        System.out.print("value 0" + suitcase_number + ": " + valuesSuitcases[i]);
+      }
+
+      else
+      {
+        System.out.print("value " + suitcase_number + ": " + valuesSuitcases[i]);
+      }
+      System.out.print(" | ");
+
+      if(suitcase_number == 5 || suitcase_number == 10 || suitcase_number == 15 || suitcase_number == 20 || suitcase_number == 25)
+      {
+        System.out.print("\n");
+        System.out.print("| ");
+      }
+
+      suitcase_number++;
+    }
+    System.out.println("\n------------------------------------------------------------------------------------------------");
+
+    System.out.println("\n\nPlease choose a Suitcase:");
+    int valueChoice = user_input.nextInt();
+  }
+
   public static void user_choice(Scanner user_input)
   {
     System.out.println("\nWas moechten Sie tun:\n\nSpiel starten = 1\nAnwendung beenden = 2\nBitte geben Sie ihre Auswahl ein: ");
@@ -52,14 +104,6 @@ public class deal_or_no_deal
         System.out.println("wrong input\n");
       }
     }
-  }
-
-  public static void table(Scanner user_input)
-  {
-    System.out.println("\nSuitcases:");
-
-    System.out.println("\nPlease choose a Suitcase:");
-    int valueChoice = user_input.nextInt();
   }
   
   public static void offer_calc()
